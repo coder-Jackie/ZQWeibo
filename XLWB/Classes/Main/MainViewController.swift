3ee22f18b20b6e57a13cb3620b0ffe33137b0e9b
CoderZQ <447017500@qq.com>
2019-08-28 17:53:44 +0800

    添加Tabbar控制器

--- XLWB/Classes/Main/MainViewController.swift
+++ XLWB/Classes/Main/MainViewController.swift
@@ -13,18 +13,30 @@ class MainViewController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tabBar.tintColor = UIColor.orange
+        
+        addChildViewController(childController: HomeTableViewController(), title: "首页", imageName: "tabbar_home")
+        addChildViewController(childController: HomeTableViewController(), title: "消息", imageName: "tabbar_message_center")
+        addChildViewController(childController: HomeTableViewController(), title: "广场", imageName: "tabbar_discover")
+        addChildViewController(childController: HomeTableViewController(), title: "我的", imageName: "tabbar_profile")
+    }
+    
+    
+    
+    
+    /// 初始化子控制器
+    private func addChildViewController(childController:UIViewController,title:String,imageName:String)  {
+        
+        childController.tabBarItem.image = UIImage.init(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage.init(named: imageName + "_highlighted")
+        childController.tabBarItem.title = title
+        childController.title = title
+        let nav = UINavigationController()
+        nav.addChild(childController)
+        addChild(nav)
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
